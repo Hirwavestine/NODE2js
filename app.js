1f93e8ded7b8eb91bafca6cee489006c59bda2e0
hirwavestine <gilhirwa22@gmail.com>
2019-04-15 09:25:52 +0200

    connecting to mongoDb using an ODM-mongoose

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-let MongoClient = require("mongodb").MongoClient;
-MongoClient.connect("mongodb://localhost:27017/animals", function(err, db) {
-  if (err) return err;
-  console.log("CONNECTED");
-});
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/animals", { useMongoClient: true });
+mongoose.connection.once("open", () => console.log("CONNECTED")).on("error"),
+  err => {
+    console.log(`could not connect`, err);
+  };
 
 //const mongoose = require("mongoose");
+//mongoose.Promise = global.Promise;
+//mongoose
+// .connect("mongodb://localhost:27017/animals")
+// .then(db => {
+//  console.log("MONGO CONNECTED");
+// })
+//.catch(err => {
+//   console.log("Error");
+// });
 
-//mongoose.connect("mongodb://localhost:27017/animals", { useMongoClient: true });
-//mongoose.connection
-//.once("open", () => console.log("CONNECTED"))
-//.on("error", err => {
-//console.log(`could not connect`, err);
-//  });
+//let MongoClient = require("mongodb").MongoClient;
+//MongoClient.connect("mongodb://localhost:27017/animals", function(err, db) {
+//  if (err) return err;
+// console.log("CONNECTED");
+//});
