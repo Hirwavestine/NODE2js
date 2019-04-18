747c86dcadab5085c5332442cd7746388163cb84
hirwavestine <gilhirwa22@gmail.com>
2019-04-18 09:50:34 +0200

    setting up express

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const User = require("./models/User");
+const express = require("express");
+const app = express();
 
 //mongoose.Promise = global.Promise;
 
@@ -14,13 +16,20 @@ mongoose.connection
     console.log(`could not connect`, err);
   });
 
-const newUser = new User({
-  firstName: "Berwa",
-  lastName: "Amaris",
-  middleName: "Davinah"
-});
+// const newUser = new User({
+//   firstName: "Berwa",
+//   lastName: "Amaris",
+//   middleName: "Davinah"
+// });
 
-newUser.save(function(err, dataSaved) {
-  if (err) return console.log(err);
-  console.log(dataSaved);
+// newUser.save(function(err, dataSaved) {
+//   if (err) return console.log(err);
+//   console.log(dataSaved);
+// });
+
+//process.env.PORT we were saying either use a port that we have available in the environment whatever
+//environment that is or use our local environment or local port
+const port = 4444 || process.env.PORT;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
