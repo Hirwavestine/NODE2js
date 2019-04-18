53f624a7b0100d50fa5733fa0e15afb3a9b7ea02
hirwavestine <gilhirwa22@gmail.com>
2019-04-18 11:23:23 +0200

    saving raw data

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const User = require("./models/User");
 const express = require("express");
 const app = express();
 
-//mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
 mongoose.connect("mongodb://localhost:27017/mongoose", {
   useNewUrlParser: true
@@ -20,17 +20,17 @@ app.get("/", (req, res) => {
   res.send("ROOT");
 });
 //create a route that is going to take a post request and is going to take a post request going to users
-app.post("/users", (req, res) => {});
-
-const newUser = new User({
-  firstName: "Berwa",
-  lastName: "Amaris",
-  middleName: "Davinah"
-});
-
-newUser.save(function(err, dataSaved) {
-  if (err) return console.log(err);
-  console.log(dataSaved);
+app.post("/users", (req, res) => {
+  const newUser = new User({
+    firstName: "Sangwas",
+    lastName: "Arsened",
+    middleName: "Davin"
+  });
+  //then is like execute
+  newUser.save().then(savesdUser => {
+    //console.log("saved user");
+    res.send("USER SAVED");
+  });
 });
 
 //process.env.PORT we were saying either use a port that we have available in the environment whatever
